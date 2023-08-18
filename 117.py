9022ec2365b599a806450b544a4220a62513162c
mohamad sadegh mohamadi <82243525+sadegh-msm@users.noreply.github.com>
2023-08-18 21:20:25 +0330

    add question 117

--- 117.py
+++ 117.py
@@ -1,4 +1,7 @@
 # Definition for a Node.
+from collections import deque
+
+
 class Node:
     def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
         self.val = val
@@ -6,5 +9,21 @@ class Node:
         self.right = right
         self.next = next
 
+
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
+        if root is None:
+            return root
+        q = deque([root])
+        while q:
+            prev = None
+            for _ in range(len(q)):
+                cur = q.popleft()
+                if prev is not None:
+                    prev.next = cur
+                prev = cur
+                if cur.left is not None:
+                    q.append(cur.left)
+                if cur.right is not None:
+                    q.append(cur.right)
+        return root
